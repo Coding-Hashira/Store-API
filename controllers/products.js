e410c956382a7fb729435679ca0032d5b4ed0a85
Coding-Hashira <96920778+Coding-Hashira@users.noreply.github.com>
2022-03-15 19:37:50 +0530

    After Adding Sort, Select And Pagination.

--- controllers/products.js
+++ controllers/products.js
@@ -1,17 +1,15 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const search = "phone";
-  const products = await Product.find({
-    name: { $regex: search, $options: "i" },
-  });
-  res.status(200).json({ products, productCount: products.length });
+  const products = await Product.find({}).select("name price").limit(5);
+  res.status(200).json({ productCount: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
+  // Search
   if (featured) {
     queryObject.featured = featured ? true : false;
   }
@@ -21,9 +19,30 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
-  res.status(200).json({ products, productCount: products.length });
+  let result = Product.find(queryObject); // list of products
+
+  // Sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  // Select
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  // Pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result.skip(skip).limit(limit);
+
+  const products = await result;
+  res.status(200).json({ productCount: products.length, products });
 };
 
 module.exports = {
