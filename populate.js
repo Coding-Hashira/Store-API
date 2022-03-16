cb0ad526fb80c4e0d695d3abe293482710cc9529
Coding-Hashira <96920778+Coding-Hashira@users.noreply.github.com>
2022-03-16 16:16:41 +0530

    After removing old products, adding better ones

--- populate.js
+++ populate.js
@@ -8,9 +8,10 @@ const dummyProducts = require("./products.json");
 const start = async () => {
   try {
     await connectToDb(process.env.MONGO_URI);
+    console.log("Connected To Mongo!");
     await Product.deleteMany();
     await Product.create(dummyProducts);
-    console.log("Success!!");
+    console.log("Added Products!");
     process.exit(0);
   } catch (error) {
     console.log(error);
